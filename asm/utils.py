52fd89bd1719e0711d3e1951c7944181b255e679
fbigun <olutyo@gmail.com>
2019-07-22 16:29:27 +0800

    增加 urlsafe 的 base64 编码解码方法

--- asm/utils.py
+++ asm/utils.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from base64 import urlsafe_b64decode, urlsafe_b64encode
+from base64 import b64decode, b64encode, urlsafe_b64decode, urlsafe_b64encode
 from urllib.parse import quote, unquote
+from functools import partial
 
 
-def Base64encode(text):
-    base64Str = urlsafe_b64encode(text.encode()).decode()
+def Base64encode(text, method=b64encode):
+    base64Str = method(text.encode()).decode()
 
     return base64Str
 
 
-def Base64decode(base64Str):
+def Base64decode(base64Str, method=b64decode):
     # base64 decode should meet the padding rules
     s_padding = ''
     n = len(base64Str) % 3
@@ -19,10 +20,15 @@ def Base64decode(base64Str):
     elif n == 2:
         s_padding = '='
     base64Str_padding = base64Str + s_padding
+    print(base64Str_padding)
 
-    text = urlsafe_b64decode(base64Str_padding.encode()).decode()
+    text = method(base64Str_padding.encode()).decode()
 
     return text
+
+
+urlsafe_base64decode = partial(Base64decode, method=urlsafe_b64decode)
+urlsafe_base64encode = partial(Base64encode, method=urlsafe_b64encode)
 
 
 def urlencode(text):

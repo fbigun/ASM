b35ca070d5ee29fa7163cff5acc38c164b52ccdd
fbigun <olutyo@gmail.com>
2019-07-27 11:23:42 +0800

    fix: 修复 Base64 解码逻辑

--- asm/utils.py
+++ asm/utils.py
@@ -3,6 +3,8 @@ from base64 import b64decode, b64encode, urlsafe_b64decode, urlsafe_b64encode
 from urllib.parse import quote, unquote
 from functools import partial
 
+from .exceptions import Base64Error
+
 
 def Base64encode(text, method=b64encode):
     base64Str = method(text.encode()).decode()
@@ -13,14 +15,15 @@ def Base64encode(text, method=b64encode):
 def Base64decode(base64Str, method=b64decode):
     # base64 decode should meet the padding rules
     s_padding = ''
-    n = len(base64Str) % 3
+    n = len(base64Str) % 4
 
     if n == 1:
-        s_padding = '=='
+        raise Base64Error('非法字符串,不是一个可解码的字符串')
     elif n == 2:
+        s_padding = '=='
+    elif n == 3:
         s_padding = '='
     base64Str_padding = base64Str + s_padding
-    print(base64Str_padding)
 
     text = method(base64Str_padding.encode()).decode()
 
@@ -35,5 +38,5 @@ def urlencode(text):
     return quote(text)
 
 
-def urldecode(text):
-    return unquote(text)
+def urldecode(urlStr):
+    return unquote(urlStr)

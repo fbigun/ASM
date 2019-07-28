d48334d08957c06c2a9424c6850dd89bf3238fb8
fbigun <olutyo@gmail.com>
2019-07-28 11:53:46 +0800

    feature: 添加异常 EnvError、 SchemesError
    - 环境变量设置不正确或漏写等提示环境变量异常
    - 数据库连接协议不受支持

--- asm/exceptions.py
+++ asm/exceptions.py
@@ -2,5 +2,17 @@
 from binascii import Error
 
 
-class Base64Error(Error):
+class BaseError(Error):
     """一个模棱两可的异常"""
+
+
+class Base64Error(BaseError):
+    """base64 编码解码异常"""
+
+
+class EnvError(BaseError):
+    """环境变量未设置或设置错误"""
+
+
+class SchemesError(BaseError):
+    """连接协议不支持"""
